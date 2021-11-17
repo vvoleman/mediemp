5841e566cc007d87e7eb507a55635ee2b5f9b3cb
vvoleman <vojtech.voleman@tul.cz>
2021-11-17 12:43:46 +0100

    yet another stupid mistake

---
+++ src/Extension/InstanceOfExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Extension;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class InstanceOfExtension extends AbstractExtension {
+
+    public function getFunctions(): array {
+        return [
+            new TwigFunction('instanceof', [$this, 'isInstanceOf']),
+        ];
+    }
+
+    public function isInstanceOf($var, $instance) {
+        $reflexionClass = new \ReflectionClass($instance);
+        return $reflexionClass->isInstance($var);
+    }
+
+}
