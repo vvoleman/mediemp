5cd6a1a2b6746ffb0d6f315fa51d902a9a21b3d7
Jakub Ruta <jakub.ruta@tul.cz>
2021-11-19 14:10:49 +0100

    Add appointnemt

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controller;
 
+use App\Entity\CourseAppointment;
+use App\Entity\EmployerCourse;
 use App\Entity\GlobalCourse;
+use App\Repository\EmployerCourseRepository;
 use App\Repository\GlobalCourseRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,13 +26,46 @@ class CoursesController extends AbstractController
         ]);
     }
 
-    #[Route('/courses/{id}', name: 'app_courses_one')]
+
+    /**
+     * @Route("/courses/{id}", name="app_courses_one",methods={"GET"})
+     */
     public function index2(GlobalCourseRepository $repository, GlobalCourse $c): Response
     {
-
+        $courses = $c->getEmployerCourses()->toArray();
         return $this->render('courses/one.html.twig', [
             'controller_name' => 'CoursesController',
-            'course' => $c
+            'course' => $c,
+            'emp_course' => $courses,
+        ]);
+    }
+    /**
+     * @Route("/courses/{id}", name="app_courses_one",methods={"POST"})
+     */
+    public function index2Post(GlobalCourseRepository $repository, GlobalCourse $c): Response
+    {
+
+        $url = $this->previousUrlService->get();
+        if (!$url) {
+            $url = $this->generateUrl("app_home");
+        }
+
+        return new RedirectResponse($url);
+    }
+
+
+    #[Route('/courses/{c_id}/appointment/{id}', name: 'app_courses_one_appointment')]
+    public function indexAppointment(CourseAppointment $c): Response
+    {
+        $course = $c->getEmployerCourse()->getCourse();
+        $peoples = $c->getCourseRegistrations()->toArray();
+
+        return $this->render('courses/appointment.html.twig', [
+            'controller_name' => 'CoursesController',
+            'course' => $course,
+            'peoples' => $peoples,
+            'appointment' => $c
+
         ]);
     }
 }
