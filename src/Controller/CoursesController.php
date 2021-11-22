123ecdc02be76bafa32694fe3d3eb33839614459
Jakub Ruta <jakub.ruta@tul.cz>
2021-11-22 09:58:14 +0100

    add edit for epointment in view

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -6,6 +6,7 @@ use App\Entity\CourseAppointment;
 use App\Entity\EmployerCourse;
 use App\Entity\GlobalCourse;
 use App\Repository\EmployerCourseRepository;
+use App\Repository\EmployerRepository;
 use App\Repository\GlobalCourseRepository;
 use App\Service\Util\PreviousUrlService;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -32,28 +33,39 @@ class CoursesController extends AbstractController
     /**
      * @Route("/courses/{id}", name="app_courses_one",methods={"GET"})
      */
-    public function index2(GlobalCourseRepository $repository, GlobalCourse $c): Response
+    public function index2(EmployerRepository $em, GlobalCourse $c): Response
     {
+        $all_emplyers = $em->findAll();
         $courses = $c->getEmployerCourses()->toArray();
         return $this->render('courses/one.html.twig', [
             'controller_name' => 'CoursesController',
             'course' => $c,
             'emp_course' => $courses,
+            "emp_all" => $all_emplyers
         ]);
     }
     /**
      * @Route("/courses/{id}", name="app_courses_one_post",methods={"POST"})
      */
-    public function index2Post(GlobalCourse $c, Request $request, PreviousUrlService $previousUrlService, $id): Response
+    public function index2Post(GlobalCourse $c, Request $request, $id): Response
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $c->setName($request->request->get('name'));
-        $c->setFocus($request->request->get('focus'));
-        $c->setSpecialization($request->request->get('specialization'));
-        $c->setKeywords($request->request->get('keywords'));
-        $entityManager->persist($c);
-        $entityManager->flush();
-        $this->addFlash("success", "Data saved");
+        if ($request->request->get('action') == "edit_course") {
+            $entityManager = $this->getDoctrine()->getManager();
+            $c->setName($request->request->get('name'));
+            $c->setFocus($request->request->get('focus'));
+            $c->setSpecialization($request->request->get('specialization'));
+            $c->setKeywords($request->request->get('keywords'));
+            $entityManager->persist($c);
+            $entityManager->flush();
+            $this->addFlash("success", "Data saved");
+        } else if ($request->request->get('action') == "create_appointment") {
+            /*$entityManager = $this->getDoctrine()->getManager();
+            $new = new CourseAppointment();
+            //$new->setEmployerCourse($)
+            $entityManager->persist($c);
+            $entityManager->flush();
+            $this->addFlash("success", "Data saved");*/
+        }
         return new RedirectResponse($this->generateUrl("app_courses_one", ['id' => $id]));
     }
 
@@ -63,7 +75,7 @@ class CoursesController extends AbstractController
     {
         $course = $c->getEmployerCourse()->getCourse();
         $peoples = $c->getCourseRegistrations()->toArray();
-
+        //dd($peoples);
         return $this->render('courses/appointment.html.twig', [
             'controller_name' => 'CoursesController',
             'course' => $course,
