0587ec3aec6438f25b7f08eae1208d9db9b4f975
Jakub Ruta <jakub.ruta@tul.cz>
2021-11-30 17:08:58 +0100

    add edit appointment date, place, cappacity

--- src/Controller/CoursesController.php
+++ src/Controller/CoursesController.php
@@ -9,6 +9,7 @@ use App\Repository\EmployerCourseRepository;
 use App\Repository\EmployerRepository;
 use App\Repository\GlobalCourseRepository;
 use App\Service\Util\PreviousUrlService;
+use DateTime;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Doctrine\ORM\Mapping\Id;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,9 +20,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class CoursesController extends AbstractController
 {
     #[Route('/courses', name: 'app_courses')]
-    public function index(GlobalCourseRepository $repository): Response
+    public function indexCourses(GlobalCourseRepository $repository): Response
     {
-
         $records = $repository->findAll();
         return $this->render('courses/index.html.twig', [
             'controller_name' => 'CoursesController',
@@ -33,7 +33,7 @@ class CoursesController extends AbstractController
     /**
      * @Route("/courses/{id}", name="app_courses_one",methods={"GET"})
      */
-    public function index2(EmployerRepository $em, GlobalCourse $c): Response
+    public function indexOneCourse(EmployerRepository $em, GlobalCourse $c): Response
     {
         $all_emplyers = $em->findAll();
         $courses = $c->getEmployerCourses()->toArray();
@@ -47,7 +47,7 @@ class CoursesController extends AbstractController
     /**
      * @Route("/courses/{id}", name="app_courses_one_post",methods={"POST"})
      */
-    public function index2Post(GlobalCourse $c, Request $request, $id): Response
+    public function indexOneCoursePost(GlobalCourse $c, Request $request, $id): Response
     {
         if ($request->request->get('action') == "edit_course") {
             $entityManager = $this->getDoctrine()->getManager();
@@ -69,8 +69,9 @@ class CoursesController extends AbstractController
         return new RedirectResponse($this->generateUrl("app_courses_one", ['id' => $id]));
     }
 
-
-    #[Route('/courses/{c_id}/appointment/{id}', name: 'app_courses_one_appointment')]
+    /**
+     * @Route("/courses/{c_id}/appointment/{id}", name="app_courses_one_appointment",methods={"GET"})
+     */
     public function indexAppointment(CourseAppointment $c): Response
     {
         $course = $c->getEmployerCourse()->getCourse();
@@ -83,5 +84,28 @@ class CoursesController extends AbstractController
             'appointment' => $c
 
         ]);
+    }
+    /**
+     * @Route("/courses/{c_id}/appointment/{id}", name="app_courses_one_appointment_post",methods={"POST"})
+     */
+    public function indexAppointmentPost(CourseAppointment $c, Request $request, $c_id, $id): Response
+    {
+        if ($request->request->get('action') == "edit_course") {
+            $entityManager = $this->getDoctrine()->getManager();
+            $c->setDate(new DateTime(date("Y-m-d H:i:s", strtotime($request->request->get('date')))));
+            $c->setPlace($request->request->get('place'));
+            $c->setCapacity($request->request->get('capacity'));
+            $entityManager->persist($c);
+            $entityManager->flush();
+            $this->addFlash("success", "Data saved");
+        } else if ($request->request->get('action') == "create_appointment") {
+            /*$entityManager = $this->getDoctrine()->getManager();
+            $new = new CourseAppointment();
+            //$new->setEmployerCourse($)
+            $entityManager->persist($c);
+            $entityManager->flush();
+            $this->addFlash("success", "Data saved");*/
+        }
+        return new RedirectResponse($this->generateUrl("app_courses_one_appointment", ['id' => $id, 'c_id' => $c_id]));
     }
 }
